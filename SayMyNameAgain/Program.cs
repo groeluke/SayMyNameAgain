d046fb06d6acca5f88cc88fadccbb34149ca6aaa
lukeg <groeluke@isu.edu>
2026-01-27 08:51:05 -0700

    Add project files.

---
+++ SayMyNameAgain/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SayMyNameAgain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // say my name again
+
+            string userInput = "";
+            int result = 0;
+            bool isValid = false;
+            string message = "";
+
+            do
+            {
+                Console.WriteLine("Please enter your name:");
+                userInput = Console.ReadLine();
+                Console.WriteLine($"{userInput}, have an amazing day!");
+
+                if (userInput == "Luke" || userInput == "luke")
+                { }
+
+
+
+            } while (!isValid);
+        }
+    }
+}
